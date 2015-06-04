5b4c16b3c56ab781d9296d33319623e50289a885
makafis <vinnyalfieri@gmail.com>
2015-06-04 17:07:59 -0400

    complete

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+ # length_of_array = words.length
+  counter = 0
+    while counter < words.length
+        yield(words[counter])
+        counter += 1
+    end 
+    words
 end
